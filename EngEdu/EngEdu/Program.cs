cf10f23198b4a4e91bb869cbd8601ffaead989ed
iomxin <aomsin254161@gmail.com>
2018-09-17 11:54:17 +0700

    2nd  year semester 1

--- EngEdu/EngEdu/Program.cs
+++ EngEdu/EngEdu/Program.cs
@@ -12,6 +12,7 @@ namespace EngEdu
         {
             Console.WriteLine("Year 255x semeter 1 , i am an EngEdu freshman.");
             Console.WriteLine("Year 255x semeter 2, i learn to program in c sharp.");
+            Console.WriteLine("Year 2560 semeter 1, i start my programmer life.");
         }
     }
 }
